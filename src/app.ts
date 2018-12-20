799e069c5219c10526c58f2711d087e5186f1020
kng83 <pawel.kng@gmail.com>
2018-12-20 17:50:49 +0100

    some

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
 import * as express from 'express';
+import {join} from 'path';
+
 
 const app = express();
 const port = 3000;
 
-app.get('/' ,(req,res)=> res.send('hi there'));
+
+
+app.get('/', function(req, res) {
+    const path = join(__dirname , '../www/index.html');
+    // console.log(path);
+    res.sendFile(path);
+});
+
+app.get('/dist/bin/main.js',function(req,res){
+    const path = join(__dirname,'./bin/main.js')
+    // console.log(path);
+    res.sendFile(path); 
+});
+
+app.get('/js/require.js',function(req,res){
+    const path = join(__dirname,'../www/js/require.js')
+    console.log(path);
+    res.sendFile(path); 
+});
+
 app.listen(port , ()=>console.log(`Listen on port ${port}`))
---
+++ src/bin/main.ts
@@ -0,0 +1,8 @@
+// import {interval} from 'rxjs';
+
+// interval(1000).subscribe(value => console.log(value));
+
+import {select} from 'd3-selection';
+
+let me = select('body');
+console.log(me);
