412aacd92898d8cd01b02450163603af360cb90b
kng83 <pawel.kng@gmail.com>
2018-12-20 16:29:48 +0100

    some

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,7 @@
-//some tekst
-console.log('somessddd');
+import * as express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/' ,(req,res)=> res.send('hi there'));
+app.listen(port , ()=>console.log(`Listen on port ${port}`))
