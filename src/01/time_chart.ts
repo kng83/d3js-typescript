a4c36a9f9d3a91089e7c91422117ff13850c6921
kng83 <pawel.kng@gmail.com>
2018-12-28 18:20:11 +0100

    some

--- src/01/time_chart.ts
+++ src/01/time_chart.ts
@@ -3,11 +3,10 @@ import { interval, pipe } from 'rxjs';
 import { map, distinctUntilChanged, take } from 'rxjs/operators';
 
 let html = document.getElementById('myChart')
+console.log(html);
 let ctx = (<any>html).getContext('2d')
 
-let newDate = new Date() as any as number;
-
-
+let newDate = Date.now();
 
 let fillArray = (numberOfElements: number, pattern: Function) => {
     let arr = []
@@ -33,10 +32,10 @@ let chart = new Chart(ctx, {
         datasets: [{
             label: "Point",
             // backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(155, 99, 132)',
+            borderColor: 'rgba(66, 134, 244,0.7)',
             steppedLine: "before",
             data: vData,
-            fill: false,
+            fill: true,
         }]
     },
 
@@ -83,6 +82,10 @@ let chart = new Chart(ctx, {
     }
 });
 
+let pause = false;
+let counter = 0;
+let recursionRunning = false;
+
 interval(1000).pipe(
     map((value) => {
         let random = Math.round(Math.random() + 0.0)
@@ -90,16 +93,72 @@ interval(1000).pipe(
     }),
     // distinctUntilChanged()
 ).subscribe(value => {
-    let newDate = +new Date();
-    chartData.push({ x: newDate, y: value });
-    // vData.push(chartData[chartData.length -1])
-    // vData.shift();
+    let newDate = Date.now();
+    let pushObj = { x: newDate, y: value }
+    chartData.push(pushObj);
+    if (!pause) {
+        //check counter
+        let updated = updateChart(counter);
+        if (updated == false) {
+            vData.push(chartData[chartData.length - 1])
+            vData.shift();
+            chart.update();
+        }
+        if (updated && !recursionRunning) counter = 0;
+        if (recursionRunning) counter++;
 
-    chart.data.datasets.forEach(dataset=> {
-        dataset.data.forEach((d,index)=>{
-            console.log(d);
-            d[index] = chartData[chartData.length - 1 - d.length   + index]
-    });
+    }
+    else {
+        console.log(counter, pushObj);
+        counter++;
+    }
+
 })
+
+function updateChart(counter) {
+    if (counter == 0) return false;
+    if (recursionRunning) return true;
+
+    recursionRunning = true;
+    return function recursion(counter) {
+
+        vData.push(chartData[chartData.length - 1 - counter])
+        vData.shift();
+        counter--;
+        if (counter <= 0) {
+            recursionRunning = false;
+        } else {
+            console.log(counter);
+            return recursion(counter)
+        }
+        if (!recursionRunning) return true;
+    }(counter);
+}
+
+
+//*** Button make up */
+document.getElementById('zoomOut').addEventListener("click", () => {
+    console.log(vData.length, chartData.length);
+    vData.unshift(chartData[chartData.length - 1 - vData.length]);
     chart.update();
+})
+
+let holdInterval;
+document.getElementById('zoomIn').addEventListener("click", (event) => {
+    function makeIt(){
+        console.log('zoomIn','event');
+        console.log(vData.length, chartData.length);
+        vData.shift();
+        chart.update();
+    }
+    holdInterval =setInterval(()=>{
+        makeIt();
+    },500)
+})
+document.body.addEventListener("mousedown", (event) => {
+    clearInterval(holdInterval);
+})
+document.getElementById('pause').addEventListener("click", () => {
+    console.log('pause')
+    pause = !pause;
 })
