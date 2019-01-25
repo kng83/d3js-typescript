b4ec64992bd5701c650f7a49e4d13f218901592e
kng83 <pawel.kng@gmail.com>
2019-01-25 13:02:36 +0100

    some

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
  import {interval} from 'rxjs';
-
  interval(1000).subscribe(value => console.log(value));
 
 
-import {select} from 'd3-selection';
 
-// let me = select('body');
-console.log('mes');
+
