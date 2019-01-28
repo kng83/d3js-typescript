828da9d0881577ac2f819d3dfc6081ad30009a3d
kng83 <pawel.kng@gmail.com>
2019-01-28 18:53:36 +0100

    some

--- app.js
+++ app.js
@@ -14,4 +14,26 @@ app.get('/dist/bundle.js', function (req, res) {
     res.sendFile(p);
 });
 
+
+//**** Check data base
+const sql = require('mssql');
+
+(async () => {
+    try {
+        await sql.connect('mssql://sa:12345@localhost:49714/Pyszczek');
+        const result = await sql.query(
+            `SELECT TOP 1000 
+                [Name],[Age]
+                FROM [Pyszczek].[dbo].[First]`);
+        console.log(result.recordset);
+
+        // sql.query(`INSERT INTO
+        //     [Pyszczek].[dbo].[First]
+        //     VALUES (Zuzia, 1)`)
+    } catch (err) {
+        // ... error checks
+        console.log(err.message);//
+    }
+})();
+
 app.listen(port, () => console.log("Listen on port " + port))
